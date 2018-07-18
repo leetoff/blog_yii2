369291f90639a3c6cd7e0a73764adeeac788c1f4
leetoff <leetoff1337@gmail.com>
2018-07-18 17:55:42 +0300

    comments tree

--- frontend/views/blog/view.php
+++ frontend/views/blog/view.php
@@ -28,22 +28,46 @@ $this->params['breadcrumbs'][] = $this->title;
     <?= $this->render('_commentform', [
         'model1' => $model1,
     ]) ?>
-        <?php $comments = Comments::find()->where(['arcticle_id'=>$model->id])->all();?>
-        <ul>
-            <?php foreach($comments as $comment){?>
-            <li class="comment">
-                <b><?= Html::encode(CommentsRepos::getUserName($comment));?></b>
+            <ul>
+                <?php buildcomment($model->id);?>
+            </ul>
+            <!-- Подключаем jquery с сервера Яндекса -->
+            <script type="text/javascript" src="http://yandex.st/jquery/1.7.1/jquery.min.js"></script>
+    <!-- Наш скрипт запроса и обработки -->
+    <script type="text/javascript">
+        $(document).ready(function() {
+            // Вешаем обработчик события "клик" на все ссылки с классом ajax_link
+            $('button.ajax_link').click(function() {
+                // Берем действие из атрибута data-action ссылки
+                var parent = $(this).data('parent');
+                var level = $(this).data('level');
+                // query append
+                        $('#comments-parent_id').val(parent);
+                        $('#comments-level').val(level);
+            })
+        })
+    </script>
+    <?php
+    function buildcomment($post_id,$current_id=0)
+    {
+        $comments = Comments::find()->where(['arcticle_id'=>$post_id])->all();
+        foreach ($comments as $comment){
+            if ($comment->parent_id==$current_id)
+            {?>
+                <li class="comment" style="margin-left:<?=40*$comment->level?>px">
+                    <b><?= Html::encode(CommentsRepos::getUserName($comment));?></b>
                 <i><?= Html::encode($comment->datetime);?></i>
-            <br>
+                <br>
                 <?= Html::encode($comment->text); ?>
-                <a href="#"
-                      id="<?= $comment->id?>"
-                      data-parent="<?= $comment->id?>"
-                      data-level="<?= $comment->level+1?>"
-                >Ответить</a>
-                <hr>
-            </li>
-            <?php } ?>
-        </ul>
+                <br>
+                <button class="ajax_link" id="<?= $comment->id?>"
+                 data-parent="<?= $comment->id?>"
+                    data-level="<?= $comment->level+1?>">Ответить</button>
+                </li>
+                <?php buildcomment($post_id,$comment->id);
+            }
+        }
+    } ?>
+
 
 </main>
